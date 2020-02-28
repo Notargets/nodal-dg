4ef5d5229da89da92e195da73d67e9feb800545e
Luke Lonergan <luke@alpaca.markets>
2020-02-28 09:31:22 -0800

    new

---
+++ nudg++/trunk/Include/Globals1D.h
@@ -0,0 +1,50 @@
+// Globals1D.h
+// container for (1D) global data
+//---------------------------------------------------------
+#ifndef NDG__Globals_1D_H__INCLUDED
+#define NDG__Globals_1D_H__INCLUDED
+
+#include "Global_funcs.h"
+#include "VecObj_Type.h"
+
+
+//---------------------------------------------------------
+class Globals1D
+//---------------------------------------------------------
+{
+public:
+
+  Globals1D();
+  virtual ~Globals1D();
+
+  void init();
+  void reset();
+  void clear();
+
+  int N, Nfp, Np K;
+  double  NODETOL;
+  int Nfaces;
+  DVec    r, x, VX;
+  DMat    Dr, Ds, LIFT, Drw, Dsw, MassMatrix;
+  DMat    Fx, nx, Fscale;
+  DMat    V, invV;
+  IVec    vmapB, mapB, vmapM, vmapP;
+  IVec    vmapI, vmapO, mapI, mapO;
+  IMat    Fmask, EToE, EToF;
+  DVec    rk4a, rk4b, rk4c;
+/*
+global N Nfp Np K
+global Nfaces EToE EToF
+global NODETOL
+global r x  VX
+global Dr LIFT
+global nx Fx Fscale
+global vmapM vmapP vmapB mapB Fmask
+global vmapI vmapO mapI mapO
+global rx J
+global rk4a rk4b rk4c
+global V invV
+ */
+};
+
+#endif // NDG__Globals_1D_H__INCLUDED
